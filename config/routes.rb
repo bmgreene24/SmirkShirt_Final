7428ac912904a5fb713531a374075eb60046a02c
Sylwia Nowaczyk <user@server.com>
2012-07-02 21:44:04 +0100

    trying to push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Smirkshirt::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "users/new"
 
   get "welcome/home"
@@ -8,6 +12,8 @@ Smirkshirt::Application.routes.draw do
   resources :products
   resources :users
   resources :sessions
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,7 +64,7 @@ Smirkshirt::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome", :action => "home"
+  match '/your_cart' => "carts#your_cart", :as => "your_cart"
   match '/login' => "sessions#new", :as => "login"
   match '/logout' => "sessions#destroy", :as => "logout"
 
